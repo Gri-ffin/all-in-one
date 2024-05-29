f64a564d25aeb95c0ae8461ba32b576df7547816
Gri-ffin <gameshunter359@gmail.com>
2024-05-29 19:16:27 +0100

    chore: Update _layout.tsx to use TamaguiProvider for theming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,8 @@ import { useEffect } from 'react'
 import 'react-native-reanimated'
 
 import { useColorScheme } from '@/hooks/useColorScheme'
+import config from '@/tamagui.config'
+import { TamaguiProvider } from '@tamagui/core'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
@@ -31,11 +33,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <TamaguiProvider config={config}>
       <Stack>
         <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
         <Stack.Screen name='+not-found' />
       </Stack>
-    </ThemeProvider>
+    </TamaguiProvider>
   )
 }
