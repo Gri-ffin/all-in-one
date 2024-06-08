87f6d5bb52f0dea34975806543ff9588f9acec22
Gri-ffin <gameshunter359@gmail.com>
2024-06-08 19:53:55 +0100

    update config

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,6 +1,20 @@
-import { createTamagui } from '@tamagui/core'
+import { createFont, createTamagui } from '@tamagui/core'
+import { config as defaultConfig } from '@tamagui/config'
+
+const customFont = createFont({
+  family: 'Ubuntu',
+  size: {
+    1: 36,
+    2: 28,
+    3: 24,
+    4: 18,
+    5: 16,
+    6: 12
+  }
+})
 
 const config = createTamagui({
+  ...defaultConfig,
   themes: {
     primary: {
       main: '#A2B2FC',
@@ -12,6 +26,10 @@ const config = createTamagui({
       gradient: 'linear-gradient(90deg, #A2B2FC 0%, #FFF1BE 100%)',
       gray: '#9D9D9D'
     }
+  },
+  fonts: {
+    heading: customFont,
+    body: customFont
   }
 })
 
