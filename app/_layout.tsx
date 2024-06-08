2ed833b6f191d45b3728931d2a6472d8b62af848
Gri-ffin <gameshunter359@gmail.com>
2024-06-08 19:54:01 +0100

    update config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,15 +4,13 @@ import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme'
-import config from '@/tamagui.config'
 import { TamaguiProvider } from '@tamagui/core'
+import config from '@/tamagui.config'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
     Ubuntu: require('@/assets/fonts/Ubuntu-Regular.ttf')
   })
