7afade25edb02e8140f4a570b031b16f370f6d0c
Gri-ffin <gameshunter359@gmail.com>
2024-06-10 19:01:04 +0100

    create the api function

---
+++ api/trending/api.ts
@@ -0,0 +1,8 @@
+import { MYANIMELIST_API } from '@/constants'
+import axios from 'axios'
+
+// TODO: this should accept an api url to change between providers
+export const getTrendingAnime = async () => {
+  const response = await axios.get(MYANIMELIST_API + '/top/anime?limit=10')
+  return response.data
+}
