f652386cc502d69cb18a27ccaa245cf3696c1a29
Gri-ffin <gameshunter359@gmail.com>
2024-06-10 19:01:11 +0100

    update the card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,14 @@
-import { Image, Text, YStack } from 'tamagui'
+import config from '@/tamagui.config'
+import { Ionicons } from '@expo/vector-icons'
+import { Image, Text, XStack, YStack } from 'tamagui'
 
 interface Props {
   imageSource: string
   title: string
-  studio: string
+  score: number
 }
 
-const Card = ({ imageSource, title, studio }: Props) => {
+const Card = ({ imageSource, title, score }: Props) => {
   return (
     <YStack width={120}>
       <Image
@@ -14,12 +16,14 @@ const Card = ({ imageSource, title, studio }: Props) => {
         alt={title}
         width={120}
         height={150}
+        borderRadius={15}
       />
       <Text
         fontSize='$4'
         overflow='hidden'
         whiteSpace='nowrap'
         textOverflow='ellipsis'
+        numberOfLines={1}
       >
         {title}
       </Text>
@@ -29,7 +33,13 @@ const Card = ({ imageSource, title, studio }: Props) => {
         whiteSpace='nowrap'
         textOverflow='ellipsis'
       >
-        From {studio}
+        <XStack alignItems='center'>
+          <Ionicons name='star' size={16} color='orange' />
+          <Text color={config.themes.secondary.gray} fontSize='$6'>
+            {' '}
+            {score}
+          </Text>
+        </XStack>
       </Text>
     </YStack>
   )
