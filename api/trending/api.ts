bb22cd8db2436a3ba36531c554e4f6daec79bc19
Gri-ffin <gameshunter359@gmail.com>
2024-06-11 18:38:02 +0100

    update the trending anime

--- api/trending/api.ts
+++ api/trending/api.ts
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 // TODO: this should accept an api url to change between providers
 export const getTrendingAnime = async () => {
-  const response = await axios.get(MYANIMELIST_API + '/top/anime?limit=10')
+  const response = await axios.get(
+    MYANIMELIST_API + '/top/anime?limit=10&filter=airing'
+  )
   return response.data
 }
