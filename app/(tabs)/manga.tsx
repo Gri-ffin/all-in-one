66d01a560a63e175c9fd80e0b7dcc740ecd4f594
Gri-ffin <gameshunter359@gmail.com>
2024-06-14 19:11:29 +0100

    create the query call

--- app/(tabs)/manga.tsx
+++ app/(tabs)/manga.tsx
@@ -1,10 +1,40 @@
+import { getTrendingManga } from '@/api/trending/api';
+import { MyAnimeListTrendingMangaResponse } from '@/api/trending/types';
 import Header from '@/components/Header'
 import Wrapper from "@/components/Wrapper";
 import { Ionicons } from '@expo/vector-icons';
-import { Input, XStack, Text, Button } from 'tamagui';
+import { useQuery } from '@tanstack/react-query';
+import { Input, XStack, Text, Button, View, Spinner } from 'tamagui';
 
 export default function MangaScreen() {
+  // const query = useQuery<MyAnimeListTrendingMangaResponse>({
+  //   queryKey: ['trending-manga'],
+  //   queryFn: getTrendingManga
+  // })
 
+  // let trendingSection = null
+
+  // if (query.isLoading) {
+  //   trendingSection = (
+  //     <View justifyContent='center' alignItems='center'>
+  //       <Spinner size='large' color='$blue10Light' />
+  //     </View>
+  //   )
+  // }
+
+  // if (query.isError) {
+  //   trendingSection = (
+  //     <View justifyContent='center' alignItems='center'>
+  //       <Text col='$red10Light' fontSize='$4'>
+  //         {query.error.message}
+  //       </Text>
+  //     </View>
+  //   )
+  // }
+
+  // if (query.isSuccess) {
+
+  // }
 
   return (
     <Wrapper>
