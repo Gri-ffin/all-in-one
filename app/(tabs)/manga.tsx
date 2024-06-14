82d4647f7b6c55691bb984821f50ce8293090e67
Gri-ffin <gameshunter359@gmail.com>
2024-06-14 18:44:31 +0100

    create the header

--- app/(tabs)/manga.tsx
+++ app/(tabs)/manga.tsx
@@ -1,12 +1,21 @@
 import Header from '@/components/Header'
 import Wrapper from "@/components/Wrapper";
-import { Input } from 'tamagui';
+import { Ionicons } from '@expo/vector-icons';
+import { Input, XStack, Text, Button } from 'tamagui';
 
 export default function MangaScreen() {
+
+
   return (
     <Wrapper>
       <Header />
       <Input placeholder='Seach manga' marginTop={22} />
+      <XStack alignItems='center' justifyContent='space-between' marginTop={17}>
+        <Text fontSize='$4'>Trending Manga</Text>
+        <Button>
+          <Ionicons size={28} name='ellipsis-horizontal' color='black' />
+        </Button>
+      </XStack>
     </Wrapper>
   )
 }
