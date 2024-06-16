cd683348d23951c953f9ce45652df6bef0ad4d3a
Gri-ffin <gameshunter359@gmail.com>
2024-06-16 18:47:20 +0100

    setup linking

--- components/index/RandomSection.tsx
+++ components/index/RandomSection.tsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query'
 import { Spinner, Text, View, XStack } from 'tamagui'
 import Avatar from '../Avatar'
 import config from '@/tamagui.config'
+import { Link } from 'expo-router'
+import { StyleSheet } from 'react-native'
 
 const RandomSection = () => {
   const randomAnimeQuery = useQuery<RandomAnimeResponse>({
@@ -28,34 +30,42 @@ const RandomSection = () => {
   }
 
   return (
-    <XStack
-      alignItems='center'
-      justifyContent='center'
-      marginVertical={15}
-      backgroundColor={config.themes.secondary.gradient}
-      paddingVertical={5}
-      borderRadius={60}
-    >
-      <Avatar
-        source={{
-          uri: randomAnimeQuery.data!.data.images.jpg.image_url
-        }}
-        width={50}
-        height={50}
-      />
-      <Text
-        fontSize='$6'
-        maxWidth={300}
-        numberOfLines={1}
-        overflow='hidden'
-        textOverflow='ellipsis'
-        marginLeft={6}
-        color='white'
+    <Link href='/details' asChild>
+      <XStack
+        alignItems='center'
+        justifyContent='center'
+        marginVertical={15}
+        backgroundColor={config.themes.secondary.gradient}
+        paddingVertical={5}
+        borderRadius={60}
       >
-        {randomAnimeQuery.data!.data.title}
-      </Text>
-    </XStack>
+        <Avatar
+          source={{
+            uri: randomAnimeQuery.data!.data.images.jpg.image_url
+          }}
+          width={50}
+          height={50}
+        />
+        <Text
+          fontSize='$6'
+          maxWidth={200}
+          numberOfLines={1}
+          overflow='hidden'
+          textOverflow='ellipsis'
+          marginLeft={6}
+          color='white'
+        >
+          {randomAnimeQuery.data!.data.title}
+        </Text>
+      </XStack>
+    </Link>
   )
 }
 
 export default RandomSection
+
+const styles = StyleSheet.create({
+  link: {
+    width: '100%'
+  }
+})
