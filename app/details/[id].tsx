aff2a71f32bc5bcfe539711f9b83bc9cc6687837
Gri-ffin <gameshunter359@gmail.com>
2024-06-17 19:13:10 +0100

    details page first section

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -1,12 +1,57 @@
+import { getAnimeDetails } from "@/api/details/api"
+import { AnimeResponse } from "@/api/details/types"
 import Wrapper from "@/components/Wrapper"
-import { useLocalSearchParams } from "expo-router"
-import { Text } from "tamagui"
+import { Ionicons } from "@expo/vector-icons"
+import { useQuery } from "@tanstack/react-query"
+import { useLocalSearchParams, useNavigation } from "expo-router"
+import { Spinner, Text, View, Image, XStack, Button, YStack } from "tamagui"
 
 const DetailsScreen = () => {
   const { id } = useLocalSearchParams()
+  const navigation = useNavigation()
+  const query = useQuery<AnimeResponse>({
+    queryKey: ['anime-details', id],
+    queryFn: () => getAnimeDetails(parseInt(id as string)),
+  })
+
+  if (query.isLoading) {
+    return (
+      <View justifyContent='center' alignItems='center' height='100%'>
+        <Spinner size='large' color='$blue10Light' />
+      </View>
+    )
+  }
+
+  if (query.isError) {
+    return (
+      <View justifyContent="center" alignItems="center" height='100%'>
+        <Text color='$red10Light' fontSize='$3'>{query.error.message}</Text>
+      </View>
+    )
+  }
+
   return (
     <Wrapper>
-      <Text>id: {id}</Text>
+      <XStack alignItems="center" marginTop={-15} marginHorizontal={-15}>
+        <Button onPress={() => navigation.goBack()}>
+          <Ionicons size={28} name="arrow-back" />
+        </Button>
+        <Text textAlign="center" flex={0.8} fontSize='$5'>Details</Text>
+      </XStack>
+      <XStack marginTop={20}>
+        <Image
+          width={130}
+          height={180}
+          source={{ uri: query.data?.data.images.jpg.large_image_url }}
+          borderRadius={15}
+        />
+        <YStack flexDirection="column" justifyContent="center" marginLeft={8}>
+          <Text fontSize='$4' width={150}>{query.data?.data.title}</Text>
+          <Text fontSize='$6' color='gray' marginVertical={6}>status: {query.data?.data.status}</Text>
+          <Text fontSize='$6' color='gray' marginBottom={6}>rank: #{query.data?.data.popularity}</Text>
+          <Text fontSize='$6' color='gray'>season: {query.data?.data.season || '??'}</Text>
+        </YStack>
+      </XStack>
     </Wrapper>
   )
 }
