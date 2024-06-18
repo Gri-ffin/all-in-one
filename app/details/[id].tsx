b029f67762b4f7f52f7bdf6e2f072a48f29f498e
Gri-ffin <gameshunter359@gmail.com>
2024-06-18 18:48:00 +0100

    add second section to details page

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -52,6 +52,20 @@ const DetailsScreen = () => {
           <Text fontSize='$6' color='gray'>season: {query.data?.data.season || '??'}</Text>
         </YStack>
       </XStack>
+      <XStack marginTop={30} borderWidth={0.3} borderRadius={20} width={309} marginHorizontal={-8}>
+        <YStack borderRightWidth={0.3} width="32%" alignItems="center" paddingVertical={17} justifyContent="center">
+          <Text fontSize='$3'>{query.data?.data.type}</Text>
+          <Text fontSize='$6' color='gray'>Type</Text>
+        </YStack>
+        <YStack borderRightWidth={0.3} width='34%' alignItems="center" paddingVertical={17} justifyContent="center">
+          <Text fontSize='$3'>{query.data?.data.source}</Text>
+          <Text fontSize='$6' color='gray'>Source</Text>
+        </YStack>
+        <YStack width='34%' alignItems="center" paddingVertical={17} justifyContent="center">
+          <Text fontSize='$3'>{query.data?.data.rating.split(' - ')[0]}</Text>
+          <Text fontSize='$6' color='gray'>Rating</Text>
+        </YStack>
+      </XStack>
     </Wrapper>
   )
 }
