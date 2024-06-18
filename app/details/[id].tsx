38b8e02c184e295b21b3d766898129976d4e3b12
Gri-ffin <gameshunter359@gmail.com>
2024-06-18 19:49:52 +0100

    finish the description details section

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -4,7 +4,9 @@ import Wrapper from "@/components/Wrapper"
 import { Ionicons } from "@expo/vector-icons"
 import { useQuery } from "@tanstack/react-query"
 import { useLocalSearchParams, useNavigation } from "expo-router"
-import { Spinner, Text, View, Image, XStack, Button, YStack } from "tamagui"
+import { Spinner, Text, View, Image, XStack, Button, YStack, ScrollView } from "tamagui"
+import config from '@/tamagui.config'
+import { ExternalLink } from "@/components/ExternalLink"
 
 const DetailsScreen = () => {
   const { id } = useLocalSearchParams()
@@ -45,27 +47,33 @@ const DetailsScreen = () => {
           source={{ uri: query.data?.data.images.jpg.large_image_url }}
           borderRadius={15}
         />
-        <YStack flexDirection="column" justifyContent="center" marginLeft={8}>
-          <Text fontSize='$4' width={150}>{query.data?.data.title}</Text>
+        <YStack flexDirection="column" justifyContent="center" marginLeft={8} width={150}>
+          <Text fontSize='$4'>{query.data?.data.title}</Text>
           <Text fontSize='$6' color='gray' marginVertical={6}>status: {query.data?.data.status}</Text>
           <Text fontSize='$6' color='gray' marginBottom={6}>rank: #{query.data?.data.popularity}</Text>
-          <Text fontSize='$6' color='gray'>season: {query.data?.data.season || '??'}</Text>
+          <Text fontSize='$6' color='gray' marginBottom={6}>season: {query.data?.data.season || '??'}</Text>
+          <Text fontSize='$6' color='gray' marginBottom={5}>studio: <ExternalLink style={{ color: 'blue' }} href={query.data!.data.studios[0].url}>{query.data?.data.studios[0].name}</ExternalLink></Text>
+          <Text fontSize='$6' color='gray' lineHeight={20}>genres: {query.data?.data.genres.map(genre => genre.name).join(', ') || '??'}</Text>
         </YStack>
       </XStack>
       <XStack marginTop={30} borderWidth={0.3} borderRadius={20} width={309} marginHorizontal={-8}>
         <YStack borderRightWidth={0.3} width="32%" alignItems="center" paddingVertical={17} justifyContent="center">
-          <Text fontSize='$3'>{query.data?.data.type}</Text>
+          <Text fontSize='$3' textAlign="center">{query.data?.data.type}</Text>
           <Text fontSize='$6' color='gray'>Type</Text>
         </YStack>
         <YStack borderRightWidth={0.3} width='34%' alignItems="center" paddingVertical={17} justifyContent="center">
-          <Text fontSize='$3'>{query.data?.data.source}</Text>
+          <Text fontSize='$3' textAlign="center">{query.data?.data.source}</Text>
           <Text fontSize='$6' color='gray'>Source</Text>
         </YStack>
         <YStack width='34%' alignItems="center" paddingVertical={17} justifyContent="center">
-          <Text fontSize='$3'>{query.data?.data.rating.split(' - ')[0]}</Text>
+          <Text fontSize='$3' textAlign="center">{query.data?.data.rating.split(' - ')[0]}</Text>
           <Text fontSize='$6' color='gray'>Rating</Text>
         </YStack>
       </XStack>
+      <ScrollView marginTop={30} height='$20' contentContainerStyle={{ paddingBottom: 20 }}>
+        <Text fontSize={12} lineHeight={20}>{query.data?.data.synopsis}</Text>
+        <Button marginTop={15} color='white' bg={config.themes.secondary.gradient}>Add to favorites</Button>
+      </ScrollView>
     </Wrapper>
   )
 }
