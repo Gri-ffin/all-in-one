a061c35e109ea33c3cd4593e3149a6e3b5e25e05
Gri-ffin <gameshunter359@gmail.com>
2024-06-19 20:10:59 +0100

    add character response type

--- api/details/types.ts
+++ api/details/types.ts
@@ -1,5 +1,16 @@
-import type { Anime } from "../types";
+import type { Anime, Character } from "../types";
 
 export interface AnimeResponse {
   data: Anime
+}
+
+export interface CharacterResponse {
+  data: {
+    character: Character
+    role: string
+    voice_actors: {
+      person: Character
+      language: string
+    }[]
+  }[]
 }
