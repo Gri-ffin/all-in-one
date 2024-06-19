1b1f1ee47b5f9398756eadc5fd855305ed3a12b2
Gri-ffin <gameshunter359@gmail.com>
2024-06-19 20:10:54 +0100

    add api call for characaters

--- api/details/api.ts
+++ api/details/api.ts
@@ -5,3 +5,8 @@ export const getAnimeDetails = async (id: number) => {
   const response = await axios.get(`${MYANIMELIST_API}/anime/${id}/full`)
   return response.data
 }
+
+export const getAnimeCharacters = async (id: number) => {
+  const response = await axios.get(`${MYANIMELIST_API}/anime/${id}/characters`)
+  return response.data
+}
