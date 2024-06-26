4d59e7dadaefa93ef4e4095e8068e885b0b97197
Gri-ffin <gameshunter359@gmail.com>
2024-06-26 18:37:48 +0100

    add voice actor response types

--- api/details/types.ts
+++ api/details/types.ts
@@ -37,3 +37,35 @@ interface AniManga {
   images: Image
   title: string
 }
+
+interface VoiceActor {
+  data: {
+    mal_id: number
+    url: string
+    images: Image
+    name: string
+    given_name: string
+    family_name: string
+    alternate_names: string[]
+    birthday: string
+    favorites: number
+    about: string
+    anime: {
+      position: string
+      anime: AniManga
+    }[]
+    manga: {
+      position: string
+      manga: AniManga
+    }[]
+    voices: {
+      role: string
+      anime: AniManga
+      character: Character
+    }[]
+  }
+}
+
+export interface VoiceActorResponse {
+  data: VoiceActor
+}
