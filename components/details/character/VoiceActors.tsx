587989bd690647d7f2b95292ff676cfac44d1fdd
Gri-ffin <gameshunter359@gmail.com>
2024-06-26 18:45:30 +0100

    add link to voice actor page details

--- components/details/character/VoiceActors.tsx
+++ components/details/character/VoiceActors.tsx
@@ -1,6 +1,7 @@
 import type { FullCharacterResponse } from "@/api/details/types"
 import { ScrollView, YStack, XStack, Text } from "tamagui"
 import Avatar from "@/components/Avatar"
+import { Link } from "expo-router"
 
 interface Props {
   character: FullCharacterResponse['data']
@@ -11,13 +12,15 @@ const VoiceActors = ({ character }: Props) => {
     <ScrollView height={400} marginTop={20}>
       <YStack gap={20} marginBottom={20}>
         {character.voices.map(a => (
-          <XStack key={a.person.mal_id}>
-            <Avatar source={{ uri: a.person.images.jpg.image_url }} alt={a.person.name} />
-            <YStack justifyContent="center">
-              <Text width={220} fontSize='$5' marginLeft={5}>{a.person.name}</Text>
-              <Text width={220} fontSize='$6' color='gray' marginLeft={5}>{a.language}</Text>
-            </YStack>
-          </XStack>
+          <Link href={`/details/voiceactor/${a.person.mal_id}`} key={a.person.mal_id}>
+            <XStack >
+              <Avatar source={{ uri: a.person.images.jpg.image_url }} alt={a.person.name} />
+              <YStack justifyContent="center">
+                <Text width={220} fontSize='$5' marginLeft={5}>{a.person.name}</Text>
+                <Text width={220} fontSize='$6' color='gray' marginLeft={5}>{a.language}</Text>
+              </YStack>
+            </XStack>
+          </Link>
         ))}
       </YStack>
     </ScrollView>
