22b1a7b03d6b98b7c06b50b6ca2a8b1b8767409b
Gri-ffin <gameshunter359@gmail.com>
2024-07-06 19:19:59 +0100

    rename to favorites

---
+++ app/favorites/index.tsx
@@ -0,0 +1,12 @@
+import StackHeader from "@/components/StackHeader"
+import Wrapper from "@/components/Wrapper"
+
+const FavoritesPage = () => {
+  return (
+    <Wrapper>
+      <StackHeader title="Favorites" />
+   </Wrapper>
+  )
+}
+
+export default FavoritesPage
